c68fea4f453ae1235dd8aaf0879de615c2a932d6
Esaias Tilly <et222yc@student.lnu.se>
2023-08-24 21:05:06 +0200

    Base form design

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,11 @@
+<script lang="ts">
+  import Button from '../components/Button.svelte'
+  import Input from '../components/Input.svelte'
+</script>
+
 <div class="flex flex-col gap-4 items-center justify-center h-screen">
-    <h1 class="font-semibold text-2xl">+page</h1>
-    <p class="text-gray-600">
-        This is a page inside the <code>src/routes</code> folder.
-    </p>
+  <div class="flex flex-col w-full max-w-md gap-4">
+    <Input label="Namn" placeholder="Fyll i ditt namn" value="" />
+    <Button>Skicka</Button>
+  </div>
 </div>

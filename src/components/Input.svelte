fe3a1e25d399bc9b6101a33c5208c5b3a3cc1fdf
Esaias Tilly <et222yc@student.lnu.se>
2023-08-24 21:34:14 +0200

    Welcome page

--- src/components/Input.svelte
+++ src/components/Input.svelte
@@ -1,10 +1,28 @@
 <script lang="ts">
+  import strEmpty from '$lib/strEmpty'
+
   export let label: string
   export let placeholder: string
   export let value: string
+
+  export let errorMessage: string = ''
+  $: hasError = !strEmpty(errorMessage)
 </script>
 
 <div class="flex flex-col gap-1 items-start justify-center w-full group">
-  <label for={label} class="font-semibold text-sm group-focus-within:text-pink-400 transition-colors">{label}</label>
-  <input type="text" id={label} class="border-2 border-gray-300 rounded-md p-2 w-full transition-colors focus:border-pink-400 focus:outline-none" {placeholder} bind:value />
+  <label for={label} class="font-semibold text-sm group-focus-within:text-pink-400 transition-colors" class:text-red-400={hasError}>{label}</label>
+  <input
+    type="text"
+    id={label}
+    class="border-2 border-gray-300 rounded-md p-2 w-full transition-colors focus:border-pink-400 focus:outline-none"
+    class:border-red-400={hasError}
+    class:text-red-400={hasError}
+    {placeholder}
+    bind:value
+    on:input
+  />
+
+  {#if hasError}
+    <span class="text-sm text-red-500">{errorMessage}</span>
+  {/if}
 </div>
---
+++ src/components/TextButton.svelte
@@ -0,0 +1,3 @@
+<div class="flex flex-row items-center text-pink-500 hover:text-pink-400 cursor-pointer" on:click on:keypress role="button" tabindex="0">
+  <slot />
+</div>
